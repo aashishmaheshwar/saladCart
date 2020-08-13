7ef310f17771e8f8ca1359f9d1824d8d67d48e78
unknown <aashishmaheswar@gmail.com>
2020-08-13 18:12:59 +0530

    hiding proceed to checkout in other pages

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import "./../css/MainNavigation.css";
+import { RouteComponentProps, withRouter } from "react-router-dom";
 
-const MainNavigation = () => {
+const MainNavigation: React.FC<RouteComponentProps> = ({ location }) => {
   const enableCheckout = useSelector((state) => (state as any).enableCheckout);
   const dispatch = useDispatch();
 
@@ -15,20 +16,21 @@ const MainNavigation = () => {
     <div className="App-Header__container">
       <nav className="App-Header__nav">
         <h3 className="mainnav__title">The Salad Store</h3>
-        {(enableCheckout === true || enableCheckout === false) && (
-          <button
-            className="btn btn-primary mainnav__checkoutbtn"
-            disabled={!enableCheckout}
-            onClick={checkout}
-          >
-            <FaShoppingCart />
-            &nbsp;&nbsp;Proceed to checkout
-          </button>
-        )}
+        {(enableCheckout === true || enableCheckout === false) &&
+          location.pathname === "/ingredients" && (
+            <button
+              className="btn btn-primary mainnav__checkoutbtn"
+              disabled={!enableCheckout}
+              onClick={checkout}
+            >
+              <FaShoppingCart />
+              &nbsp;&nbsp;Proceed to checkout
+            </button>
+          )}
       </nav>
       <div className="mainnav__bottompadding"></div>
     </div>
   );
 };
 
-export default MainNavigation;
+export default withRouter(MainNavigation);
