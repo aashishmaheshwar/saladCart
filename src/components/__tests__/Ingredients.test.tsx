fc2fc117974d7108f69b3796ae0f57c06d69db55
unknown <aashishmaheswar@gmail.com>
2020-08-14 16:03:07 +0530

    Ingredients - all test cases

--- src/components/__tests__/Ingredients.test.tsx
+++ src/components/__tests__/Ingredients.test.tsx
@@ -3,17 +3,13 @@ import React, { ReactElement } from "react";
 // import { render, getByText, fireEvent, screen } from "../../utils/test-utils";
 import Ingredients from "../Ingredients";
 import { createMemoryHistory } from "history";
-// import { render } from "@testing-library/react";
 import { createStore } from "redux";
 import cartReducer from "../../reducers/cart-reducer";
 import { Provider } from "react-redux";
 import { render as rtlRender, getByText } from "@testing-library/react";
 import { fireEvent } from "@testing-library/dom";
 import ReactDOM from "react-dom";
-// import { useIngredients } from "../../hooks/useIngredients";
 import * as hooks from "../../hooks/useIngredients";
-
-// jest.mock('../../hooks/useIngredients');
 
 const OPTIONS = [
   {
@@ -58,19 +54,94 @@ describe("Ingredients", () => {
 
   it("on render - renders a header with text 'Ingredients'", () => {
     const render = getRender(initialState);
-    // useIngredients.
+    const updateIngredientCount = jest.fn();
     jest
       .spyOn(hooks, "useIngredients")
       .mockImplementation(() => [
-        jest.fn(),
+        updateIngredientCount,
         { ...initialState.selectedItems },
         OPTIONS,
         false,
       ]);
-    // jest.useFakeTimers();
     const { container } = render(<Ingredients {...routeComponentPropsMock} />);
-    // jest.advanceTimersByTime(2000);
     expect(getByText(container, "Ingredients")).toBeInTheDocument();
+  });
+
+  it(`checkout action triggers page navigation to 'Checkout' page`, () => {
+    const render = getRender({ ...initialState, checkoutTrigger: true });
+    const updateIngredientCount = jest.fn();
+    jest
+      .spyOn(hooks, "useIngredients")
+      .mockImplementation(() => [
+        updateIngredientCount,
+        { ...initialState.selectedItems },
+        OPTIONS,
+        false,
+      ]);
+    spyOn(routeComponentPropsMock.history, "push");
+    const { container } = render(<Ingredients {...routeComponentPropsMock} />);
+    expect(routeComponentPropsMock.history.push).toHaveBeenCalled();
+    expect(routeComponentPropsMock.history.push).toHaveBeenCalledWith(
+      "/checkout"
+    );
+  });
+
+  it("triggers update when increment/decremnet button is clicked for any item", () => {
+    const render = getRender({ ...initialState, checkoutTrigger: true });
+    const updateIngredientCount = jest.fn();
+    jest
+      .spyOn(hooks, "useIngredients")
+      .mockImplementation(() => [
+        updateIngredientCount,
+        { ...initialState.selectedItems },
+        OPTIONS,
+        false,
+      ]);
+    const { container } = render(<Ingredients {...routeComponentPropsMock} />);
+    const firstIngredientDecrementBtn = container.querySelector(
+      'input[type="text"]'
+    )?.nextSibling;
+    fireEvent.click(firstIngredientDecrementBtn as HTMLElement);
+    expect(updateIngredientCount).toHaveBeenCalled();
+    const firstIngredientIncrementBtn = container.querySelector(
+      'input[type="text"]'
+    )?.previousSibling;
+    fireEvent.click(firstIngredientIncrementBtn as HTMLElement);
+    expect(updateIngredientCount).toHaveBeenCalled();
+  });
+
+  it("triggers update when count is changed for any ingredient", () => {
+    const render = getRender({ ...initialState, checkoutTrigger: true });
+    const updateIngredientCount = jest.fn();
+    jest
+      .spyOn(hooks, "useIngredients")
+      .mockImplementation(() => [
+        updateIngredientCount,
+        { ...initialState.selectedItems },
+        OPTIONS,
+        false,
+      ]);
+    const { container } = render(<Ingredients {...routeComponentPropsMock} />);
+    const firstIngredientText = container.querySelector('input[type="text"]');
+    fireEvent.change(firstIngredientText as HTMLElement, {
+      target: { value: "20" },
+    });
+    expect(updateIngredientCount).toHaveBeenLastCalledWith("Cucumber", "20");
+  });
+
+  it("shows loading when no ingredients are provided", () => {
+    const render = getRender({ ...initialState, checkoutTrigger: true });
+    const updateIngredientCount = jest.fn();
+    jest
+      .spyOn(hooks, "useIngredients")
+      .mockImplementation(() => [
+        updateIngredientCount,
+        { ...initialState.selectedItems },
+        null as any,
+        false,
+      ]);
+    const { container } = render(<Ingredients {...routeComponentPropsMock} />);
+    expect(getByText(container, "Loading...")).toBeInTheDocument();
   });
 });
 
