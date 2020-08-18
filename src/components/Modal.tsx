2a112284180abd4bb0beaaefc445a416980e8739
unknown <aashishmaheswar@gmail.com>
2020-08-19 00:24:09 +0530

    better responsiveness - popup and checkout button positioning

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,30 +1,7 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 import { FaWindowClose } from "react-icons/fa";
-
-const MODAL_STYLES: CSSProperties = {
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  backgroundColor: "#FFF",
-  padding: "50px",
-  zIndex: 1000,
-  width: "50%",
-  height: "75%",
-  display: "flex",
-  flexDirection: "column",
-};
-
-const OVERLAY_STYLES: CSSProperties = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: "rgba(0, 0, 0, .7)",
-  zIndex: 1000,
-};
+import "./../css/Modal.css";
 
 const Modal: React.FC<{
   open: boolean;
@@ -36,8 +13,8 @@ const Modal: React.FC<{
 
   return ReactDom.createPortal(
     <>
-      <div style={OVERLAY_STYLES} />
-      <div style={MODAL_STYLES}>
+      <div className="modal__overlay" />
+      <div className="modal__main">
         <div>
           <h3 style={{ display: "inline-block", float: "left", color: "red" }}>
             {title}
